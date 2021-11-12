060ca60dcb51079e488a69506236ce2650981838
Kenneth Wang <yunfeiw@me.com>
2021-11-12 05:40:24 +0000

    CSS polish to make whole app more united

--- client/components/carousel-slide.jsx
+++ client/components/carousel-slide.jsx
@@ -15,7 +15,7 @@ export default function CarouselSlide({
         </div>
         <div className="col-two-fifth caro-info-col">
           <a href="#"className="caro-detail-bt-row"><button className="caro-detail-button">Detail</button></a>
-          <h3 className="caro-text">{labelOne}: <span>{valueOne || 'N/A'}</span></h3>
+          <h3 className="caro-text ">{labelOne}: <span>{valueOne || 'N/A'}</span></h3>
           <h3 className="caro-text">{labelTwo}: <span>{valueTwo || 'N/A'}</span></h3>
           <h3 className="caro-text">Genre: <span>{genre || 'N/A'}</span></h3>
           <h3 className="caro-text">Style: <span>{style || 'N/A'}</span></h3>

87815f3bd13f001a33d33ca3298f5c269d301049
Kenneth Wang <yunfeiw@me.com>
2021-11-29 01:32:57 +0000

    adding post-edit component

---
+++ client/components/post-edit.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class EditPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      post: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>hello</div>
+    );
+  }
+}
